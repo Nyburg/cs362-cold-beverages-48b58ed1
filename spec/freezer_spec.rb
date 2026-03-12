c1f4533e46000ae4b796351a1b82f185394937b3
Nick Nyburg <nick@10-116-30-70.wireless.oregonstate.edu>
2026-03-12 12:01:51 -0700

    freezer_spec file written, and covered under testing. 84% test coverage map currently

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,28 @@
 require_relative '../lib/freezer'
+require_relative '../lib/item'
 
 describe 'A freezer' do
+  it 'has a default capacity and room temperature' do
+    freezer = Freezer.new
 
+    expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+  end
+
+  it 'reduces remaining capacity when an item is added' do
+    freezer = Freezer.new
+    item = Item.new('Dreamsicle', 1)
+
+    freezer.add(item)
+
+    expect(freezer.remaining_capacity).to eq(99)
+  end
+
+  it 'sets its temperature based on level' do
+    freezer = Freezer.new
+
+    freezer.set_level(5)
+
+    expect(freezer.temperature).to eq(20)
+  end
 end
