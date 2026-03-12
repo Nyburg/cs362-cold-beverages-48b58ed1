8d82b3f72a31cdc03d4ae8975c0861bddb45c0c1
Nick Nyburg <nick@10-116-30-70.wireless.oregonstate.edu>
2026-03-12 12:22:41 -0700

    chiller_spec file complete and running

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,5 +1,28 @@
 require_relative '../lib/chiller'
+require_relative '../lib/item'
 
 describe 'A chiller' do
+  it 'has a default capacity and room temperature' do
+    chiller = Chiller.new
 
+    expect(chiller.capacity).to eq(100)
+    expect(chiller.temperature).to eq(70)
+  end
+
+  it 'reduces remaining capacity when an item is added' do
+    chiller = Chiller.new
+    item = Item.new('Pizza', 5)
+
+    chiller.add(item)
+
+    expect(chiller.remaining_capacity).to eq(95)
+  end
+
+  it 'sets its temperature based on level' do
+    chiller = Chiller.new
+
+    chiller.set_level(5)
+
+    expect(chiller.temperature).to eq(45)
+  end
 end
