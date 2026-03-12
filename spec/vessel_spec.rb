270b8ff93c265e2e4a51914d83c0fda60fda7530
Nick Nyburg <nick@Mac.lan>
2026-03-11 23:49:41 -0700

    Fix vessel behavior and completed vessel spec file

--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../lib/vessel'
 
 describe 'A vessel for holding liquid' do
-
   it 'has a name and volume' do
     vessel = Vessel.new
     expect(vessel.name).to eq('FAKE')
@@ -9,15 +8,13 @@ describe 'A vessel for holding liquid' do
   end
 
   it 'is initially empty' do
-    skip
     vessel = Vessel.new('FAKE', 100)
     expect(vessel).to be_empty
   end
 
   it 'is no longer empty when we fill it' do
-    skip
     vessel = Vessel.new('FAKE', 100)
     vessel.fill
-    expect(vessel).to_not be_empty
+    expect(vessel).not_to be_empty
   end
 end
